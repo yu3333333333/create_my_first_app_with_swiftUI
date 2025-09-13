676eeca8bde52e482b640627df92f52f68628133
yu33 <candy921205@gmail.com>
2025-09-13 15:47:14 +0800

    Initial Commit

---
+++ first_app/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  first_app
+//
+//  Created by YU33 on 2025/9/13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ first_app/first_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  first_appApp.swift
+//  first_app
+//
+//  Created by YU33 on 2025/9/13.
+//
+
+import SwiftUI
+
+@main
+struct first_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
